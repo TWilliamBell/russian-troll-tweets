b79491dae5c674100c868d50fb28c6e5f11e5659
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-02 00:42:56 -0400

    Update Data_Analysis.R

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -2,7 +2,9 @@
 
 # setwd("~/Desktop/russian-troll-tweets")
 
-## This script used an earlier version of the russian-troll-tweets repository that had the tweets in one large file.  This can be done by reading the files in separately and then merging the data, however I am not in the mood to figure out LFS well enough to upload my earlier copy of the csv file so I cannot provide it.
+## This script used an earlier version of the russian-troll-tweets repository that had the tweets in one large file.  
+## This can be done by reading the files in separately and then merging the data, however I am not in the mood to 
+## figure out LFS well enough to upload my earlier copy of the csv file so I cannot provide it.
 
 library(data.table)
 library(ggplot2)
@@ -11,9 +13,17 @@ n <- nrow(Trolls)
 
 Trolls <- fread("IRAhandle_tweets.csv")
 
-CitiesTop50 <- c("New York City", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Austin", "Jacksonville", "San Francisco", "Columbus", "Fort Worth", "Indianaopolis", "Charlotte", "Seattle", "Denver", "Washington", "Boston", "El Paso", "Detroit", "Nashville", "Memphis", "Portland", "Oklahoma City", "Las Vegas", "Louisville", "Baltimore", "Milwaukee", "Albuquerque", "Tucson", "Fresno", "Sacramento", "Mesa", "Kansas City", "Atlanta", "Long Beach", "Omaha", "Raleigh", "Colorado Springs", "Miami", "Virginia Beach", "Oakland", "Minneapolis", "Tulsa", "Arlington", "New Orleans", "Wichita")
+CitiesTop50 <- c("New York City", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", 
+                 "Dallas", "San Jose", "Austin", "Jacksonville", "San Francisco", "Columbus", "Fort Worth", "Indianaopolis", 
+                 "Charlotte", "Seattle", "Denver", "Washington", "Boston", "El Paso", "Detroit", "Nashville", "Memphis", 
+                 "Portland", "Oklahoma City", "Las Vegas", "Louisville", "Baltimore", "Milwaukee", "Albuquerque", "Tucson", 
+                 "Fresno", "Sacramento", "Mesa", "Kansas City", "Atlanta", "Long Beach", "Omaha", "Raleigh", "Colorado Springs", 
+                 "Miami", "Virginia Beach", "Oakland", "Minneapolis", "Tulsa", "Arlington", "New Orleans", "Wichita")
 
-col <- c("blue", "blue", "blue", "red", "salmon", "salmon", "red", "blue", "red", "blue", "red", "salmon", "blue", "red", "red", "red", "salmon", "blue", "lightblue", "blue", "blue", "red", "salmon", "red", "red", "blue", "red", "lightblue", "red", "salmon", "lightblue", "blue", "salmon", "blue", "blue", "salmon", "red", "salmon", "blue", "red", "salmon", "lightblue", "salmon", "lightblue", "blue", "lightblue", "red", "red", "red", "red")
+col <- c("blue", "blue", "blue", "red", "salmon", "salmon", "red", "blue", "red", "blue", "red", "salmon", "blue", "red", 
+         "red", "red", "salmon", "blue", "lightblue", "blue", "blue", "red", "salmon", "red", "red", "blue", "red", "lightblue", 
+         "red", "salmon", "lightblue", "blue", "salmon", "blue", "blue", "salmon", "red", "salmon", "blue", "red", "salmon", 
+         "lightblue", "salmon", "lightblue", "blue", "lightblue", "red", "red", "red", "red")
 
 Freq <- rep(0, 50)
 
